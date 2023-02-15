5cc81f0d1e91f21d4511d5abd37b1a50ba5337ad
seob <103884763+seob171@users.noreply.github.com>
2023-02-15 21:06:36 +0900

    Initial commit

---
+++ cssprop.d.ts
@@ -0,0 +1,17 @@
+import {} from 'react';
+import { CSSProp } from 'styled-components';
+
+declare module 'react' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface DOMAttributes<T> {
+    css?: CSSProp;
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      css?: CSSProp;
+    }
+  }
+}
